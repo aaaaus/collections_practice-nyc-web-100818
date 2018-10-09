d8d4c7a113fdac0437e119777de7db26df5a6e9e
Austin Luft <ausluft@gmail.com>
2018-10-09 18:48:11 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,34 @@
+def sort_array_asc(array)
+  array.sort
+end
 
+def sort_array_desc(array)
+  array.sort.reverse
+end
+
+def sort_array_char_count(array)
+end
+
+def swap_elements
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker
+end
+
+def find_a(array)
+end
+  
+def sum_array(array)
+  sum = 0 
+  array.each do |number|
+    sum += number
+  end
+  sum
+end
+
+def add_s(array)
+end
